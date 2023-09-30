57dd943d8571372abfd6be3b8973c05df129e023
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2023-09-30 12:50:16 +0200

    feat: prisma impl

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,45 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+    PRISMA_CONFIG_TOKEN,
+    PrismaConfig,
+    prismaConfig,
+} from './configs/prisma.config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            cache: true,
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string()
+                    .valid('development', 'production', 'test')
+                    .default('production'),
+                HOST: Joi.string().default('0.0.0.0'),
+                PORT: Joi.number().default(3000),
+                DATABASE_URL: Joi.string().required(),
+            }),
+            validationOptions: {
+                abortEarly: true,
+            },
+            load: [prismaConfig],
+        }),
+        PrismaModule.forRootAsync({
+            isGlobal: true,
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => {
+                const config =
+                    configService.getOrThrow<PrismaConfig>(PRISMA_CONFIG_TOKEN);
+                return { url: config.url, verbose: true };
+            },
+            inject: [ConfigService],
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,36 @@
+import { Module, DynamicModule } from "@nestjs/common";
+import { PRISMA_OPTIONS_TOKEN } from "./prisma.consts";
+import { PrismaOptions, PrismaAsyncOptions } from "./prisma.options";
+import { PrismaService } from "./prisma.service";
+
+@Module({
+    providers: [PrismaService],
+    exports: [PrismaService],
+})
+export class PrismaModule {
+    static forRoot(options: PrismaOptions): DynamicModule {
+        return {
+            global: options.isGlobal,
+            module: PrismaModule,
+            providers: [{ provide: PRISMA_OPTIONS_TOKEN, useValue: options }, PrismaService],
+            exports: [PrismaService],
+        };
+    }
+
+    static forRootAsync(options: PrismaAsyncOptions): DynamicModule {
+        return {
+            global: options.isGlobal,
+            module: PrismaModule,
+            providers: [
+                {
+                    provide: PRISMA_OPTIONS_TOKEN,
+                    useFactory: options.useFactory,
+                    inject: options.inject,
+                },
+                PrismaService,
+            ],
+            imports: options.imports,
+            exports: [PrismaService],
+        };
+    }
+}
